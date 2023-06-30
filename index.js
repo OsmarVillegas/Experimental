15831ee3f7094332ce288df5fa8b492369c04786
OsmarVillegas <osmarvm25@gmail.com>
2023-06-30 01:19:07 -0600

    Experimental

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import app from "./app";
-import './database';
+import app from "./src/app";
+import './src/database';
 
 app.listen(process.env.PORT || 4001);
 console.log('Server listen on port', process.env.PORT || 4001);

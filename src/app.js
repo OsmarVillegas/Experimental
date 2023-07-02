ecbec84d9c9eb79fb1e74737899474631732f0d7
OsmarVillegas <osmarvm25@gmail.com>
2023-07-02 14:46:56 -0600

    Experimental_6

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ app.set('pkg', pkg)
 app.use(express.json());
 app.use(morgan('dev'));
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();

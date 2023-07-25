e42884d5053ca7978c68b5ee6f2b7e5be1b4250a
OsmarVillegas <osmarvm25@gmail.com>
2023-07-25 10:51:39 -0600

    Corrección 4

--- src/app.js
+++ src/app.js
@@ -13,14 +13,16 @@ import multer from "multer";
 const app = express()
 
 app.use((req, res, next) => {
-    res.append('Access-Control-Allow-Origin',['*']);
+    res.append('Access-Control-Allow-Origin',['http://localhost:4200','http://localhost:4200/admin']);
     res.append('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
     // res.append('Access-Control-Allow-Headers',['Content-Type']);
     next()
 })
 
 
+const whiteList = ['http://localhost:4200','http://localhost:4200/admin']
 
+app.use(cors({ origin: whiteList }))
 
 app.set('pkg', pkg)
 

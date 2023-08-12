f66a06cd2479d2218afc44359f4f412bd5ca2e7f
OsmarVillegas <osmarvm25@gmail.com>
2023-08-11 19:45:42 -0600

    Subir archivos a Mongodb V.3.8.8.6 Corrección

--- src/controllers/datosGenerales.controllers.js
+++ src/controllers/datosGenerales.controllers.js
@@ -103,7 +103,7 @@ export const updateDatosGenerales = async (req, res) => {
       { _id: await new ObjectId("649f5b46072eb717863bcd92") },
       { $set: req.body }
     );
-    // SOME SOME
+    // SOME SOME SOME
     if (result.matchedCount === 0) {
       return res.status(404).json({ message: "El dato con ese id no existe" });
     }

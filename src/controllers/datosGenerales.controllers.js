c9ebfdff278605d8613c603542cfbc60cf07dc8c
OsmarVillegas <osmarvm25@gmail.com>
2023-08-12 19:34:26 -0600

    Subir archivos a Mongodb V.3.979 Corrección

--- src/controllers/datosGenerales.controllers.js
+++ src/controllers/datosGenerales.controllers.js
@@ -100,7 +100,7 @@ export const updateDatosGenerales = async (req, res) => {
     const db = await getDb();
     const collection = await db.collection("datosgenerales");
 
-    await collection.updateOne(
+    collection.updateOne(
       { id: "1" },
       { $set: req.body }
     );

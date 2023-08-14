8e03ce30c50b5b1c9ccea4188e3bcffa829d34e5
OsmarVillegas <osmarvm25@gmail.com>
2023-08-13 20:49:56 -0600

    Subir archivos a Mongodb V.3.99 Corrección

--- src/app.js
+++ src/app.js
@@ -16,7 +16,6 @@ app.use((req, res, next) => {
     // res.append('Access-Control-Allow-Origin',['http://localhost:4200']);
     res.append('Access-Control-Allow-Origin',['https://copper-axiom-368104.web.app','http://localhost:4200']);
     res.append('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
-    // res.append('Access-Control-Allow-Headers',['Content-Type']);
     next()
 })
 
